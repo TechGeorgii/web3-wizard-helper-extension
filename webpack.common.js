f413dc9589a509c73ab23f0cad37509534625a0e
harshal-limaye <harshal.limaye.in@gmail.com>
2022-10-05 14:25:45 +0530

    create src and webpack config file

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    background: path.resolve('src/pages/Background/index.ts'),
+    contentscript: path.resolve('src/Content/index.ts')
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.(tsx|.ts)?$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css?$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/static'),
+          to: path.resolve('dist')
+        }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+}
